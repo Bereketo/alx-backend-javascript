ede41f8b8fabccf72ce84fadfa16cf18c9ad664e
Berekto <bereketalebachew87@gmail.com>
2023-04-20 01:02:00 +0300

    promise-fixes

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,8 +4,8 @@ export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
   Promise.all([photo, user])
-    .then(([result1, result2]) => {
-      console.log(`${result1.body} ${result2.firstName} ${result2.lastName}`);
+    .then((result) => {
+      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
