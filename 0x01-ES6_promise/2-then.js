a2a48218990eb05a3fd93e69d8cbc6ec8da04976
Berekto <bereketalebachew87@gmail.com>
2023-04-20 01:15:55 +0300

    2-then-fixed

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   const body = { status: 200, body: 'success' };
-  promise.then(() => body);
+  promise.then(() => {
+    console.log('Got a response from the API'); return body;
+  });
   promise.catch((error) => error);
-  promise.finally(() => console.log('Got a response from the API'));
 }
